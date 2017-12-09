05e338e59d7262ac5de36e28c97cbdd72b1026d8
Ian Walters <ian@walters.id.au>
2017-12-09 21:27:07 +1000

    all the heros and their classes

--- src/ratings.ts
+++ src/ratings.ts
@@ -4,12 +4,13 @@ export const offense: HeroRating[] = [
     { heroId: 'wv', tier: Tier.DemiGod, tags: { 'awake': 1 } },
     { heroId: 'aa', tier: Tier.DemiGod, tags: { 'awake': 1 } },
     { heroId: 'im', tier: Tier.God },
-    { heroId: 'dv', tier: Tier.Good, tags: { 'awake': 1 } },
+    { heroId: 'dv', tier: Tier.DemiGod, tags: { 'awake': 1 } },
     { heroId: 'sw', tier: Tier.Good, tags: { 'awake': 1, 'highSig': 1 } },
     { heroId: 'gr', tier: Tier.God }, 
     { heroId: 'mk', tier: Tier.God }, 
     { heroId: 'gp', tier: Tier.God },
-    { heroId: 'md', tier: Tier.Good, tags: { 'awake': 1, 'highSig': 1  } },
+    { heroId: 'bl', tier: Tier.God },
+    { heroId: 'md', tier: Tier.DemiGod, tags: { 'awake': 1  } },
     { heroId: 'hp', tier: Tier.God },
     { heroId: 'sl', tier: Tier.DemiGod, tags: { 'awake': 1 } },
     { heroId: 'sm(ts)', tier: Tier.God },
@@ -18,7 +19,13 @@ export const offense: HeroRating[] = [
     { heroId: 'rg', tier: Tier.DemiGod },
     { heroId: 'st', tier: Tier.Good, tags: { 'awake': 1 } },
     { heroId: 'pl', tier: Tier.Good, tags: { 'awake': 1 } },
-    { heroId: 'cb', tier: Tier.DemiGod }
+    { heroId: 'c', tier: Tier.DemiGod },
+
+    { heroId: 'g', tier: Tier.DemiGod },
+    { heroId: 'dm', tier: Tier.DemiGod },
+    { heroId: 'th', tier: Tier.DemiGod },
+    { heroId: 'ms', tier: Tier.DemiGod },
+    { heroId: 'mp', tier: Tier.DemiGod },
 ];
 
 export const defense: HeroRating[] = [];
